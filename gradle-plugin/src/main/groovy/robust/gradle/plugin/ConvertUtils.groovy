074e6f9774c003cd1c63b1135f855d8c67fe5f44
wangqiang <wangqiang@wangqiangdeMacBook-Pro.local>
2023-08-31 13:28:48 +0800

    fix:修复兼容问题
    Signed-off-by: wangqiang <wangqiang@wangqiangdeMacBook-Pro.local>

--- gradle-plugin/src/main/groovy/robust/gradle/plugin/ConvertUtils.groovy
+++ gradle-plugin/src/main/groovy/robust/gradle/plugin/ConvertUtils.groovy
@@ -13,8 +13,8 @@ import java.util.regex.Matcher
  */
 class ConvertUtils {
     static List<CtClass> toCtClasses(Collection<TransformInput> inputs, ClassPool classPool) {
-        List<String> classNames = new ArrayList<>()
-        List<CtClass> allClass = new ArrayList<>();
+        Set<String> classNames = new HashSet<>()
+        List<CtClass> allClass = new ArrayList<>()
         def startTime = System.currentTimeMillis()
         inputs.each {
             it.directoryInputs.each {
@@ -23,10 +23,13 @@ class ConvertUtils {
                 org.apache.commons.io.FileUtils.listFiles(it.file, null, true).each {
                     if (it.absolutePath.endsWith(SdkConstants.DOT_CLASS)) {
                         def className = it.absolutePath.substring(dirPath.length() + 1, it.absolutePath.length() - SdkConstants.DOT_CLASS.length()).replaceAll(Matcher.quoteReplacement(File.separator), '.')
-                        if(classNames.contains(className)){
-                            throw new RuntimeException("You have duplicate classes with the same name : "+className+" please remove duplicate classes ")
+                        //https://github.com/Meituan-Dianping/Robust/issues/419
+                        if (!"META-INF.versions.9.module-info".equals(className)) {
+//                            if(classNames.contains(className)){
+//                                throw new RuntimeException("You have duplicate classes with the same name : "+className+" please remove duplicate classes ")
+//                            }
+                            classNames.add(className)
                         }
-                        classNames.add(className)
                     }
                 }
             }
@@ -40,10 +43,13 @@ class ConvertUtils {
                     String className = libClass.getName();
                     if (className.endsWith(SdkConstants.DOT_CLASS)) {
                         className = className.substring(0, className.length() - SdkConstants.DOT_CLASS.length()).replaceAll('/', '.')
-                        if(classNames.contains(className)){
-                            throw new RuntimeException("You have duplicate classes with the same name : "+className+" please remove duplicate classes ")
+                        //https://github.com/Meituan-Dianping/Robust/issues/419
+                        if (!"META-INF.versions.9.module-info".equals(className)) {
+//                            if(classNames.contains(className)){
+//                                throw new RuntimeException("You have duplicate classes with the same name : "+className+" please remove duplicate classes ")
+//                            }
+                            classNames.add(className)
                         }
-                        classNames.add(className)
                     }
                 }
             }
